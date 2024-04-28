e3e9f12248598449c1ec94e8dfa488b31926748e
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-04-28 18:16:16 +0200

    fix(layout): Display year dynamicly

--- src/shared/components/layouts/Footer.tsx
+++ src/shared/components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="lg:rounded-2xl lg:bg-primary">
       <p className="text-center py-6 text-secondary">
-        © 2023 All Rights Reserved by{" "}
+        © {new Date().getFullYear()} All Rights Reserved by
         <a
           className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-500 inline-block text-transparent bg-clip-text font-bold hover:text-primary"
           href="https://loikfekkai.dev"
